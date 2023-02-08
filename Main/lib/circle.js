c8dbe2fed7769a30cd93e4eba60a0848c0e52561
Franksu <fsuriel@gmail.com>
2023-02-07 23:59:37 -0500

    add code fo circle, triangle and square

--- Main/lib/circle.js
+++ Main/lib/circle.js
@@ -2,16 +2,18 @@ const Shape = require('./shapes');
 const CLI = require('./cli');
 
 class Circle extends Shape {
-    constructor(children);
-    super(children);
+  constructor(logoText, logoColor, shapeColor) {
+    super(logoText, logoColor, shapeColor);
+  }
 
-    render() {
-        return `<svg version="1.1" 
-        width="300" height="200" 
-        xmlns="http://www.w3.org/2000/svg">
-        <circle cx="150" cy="100" r="80" fill="${this.renderInnerHtml()}" />      
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-      
-      </svg>`
-    }
+  render() {
+    return `<svg version="1.1" 
+      width = "300" height = "200" 
+      xmlns = "http://www.w3.org/2000/svg">
+      <circle cx = "150" cy = "100" r = "80" fill = "${this.shapeColor}" />      
+      <text x = "150" y = "125" font-size = "60" text-anchor = "middle" fill = "${this.logoColor}">${this.logoText}</text>      
+    </svg>`
+  }
 }
+
+module.exports = Circle
