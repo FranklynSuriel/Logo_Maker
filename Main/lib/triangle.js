202526a7f9a881a0abfa97d67ae6076b5c00d9d5
Franksu <fsuriel@gmail.com>
2023-02-09 22:46:56 -0500

    add link to video and finish proyect

--- Main/lib/triangle.js
+++ Main/lib/triangle.js
@@ -9,13 +9,14 @@ class Triangle extends Shape {
 
   // render shape
   render() {
-    return 
-    `<svg version = "1.1" 
+    return `
+      <svg version = "1.1" 
       width = "300" height="200" 
       xmlns = "http://www.w3.org/2000/svg">
       <polygon points = "10,160 210,160 105,10" fill = "${this.shapeColor}" />      
       <text x = "105" y = "135" font-size = "60" text-anchor = "middle" fill = "${this.textColor}">${this.logoText}</text>      
-    </svg>`
+      </svg>
+    `
   }
 }
 
